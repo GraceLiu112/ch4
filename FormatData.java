8290958040f097766f9ca6bf9b56ae8d33055f76
GraceLiu112 <liugrace943@gmail.com>
2026-09-22 13:04:10 -0700

    Add files via upload

---
+++ FormatData.java
@@ -0,0 +1,15 @@
+public class FormatData {
+	public static void Data(String day, int date, String month, int year){
+		System.out.println(day);
+		System.out.println(date);
+		System.out.println(month);
+		System.out.println(year);
+		System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);	
+		System.out.println("European format: " + day + " " + date + " " + month + " " + year);				
+	}
+	public static void main(String[] args){
+		Data("Tuesday", 15, "September", 2026);
+		
+	}
+
+}
