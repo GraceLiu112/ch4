e83be2fe8fdf53f7d60c5096038123b5a9ee124e
GraceLiu112 <liugrace943@gmail.com>
2026-09-23 13:08:49 -0700

    Add files via upload

---
+++ Multadd.java
@@ -0,0 +1,14 @@
+public class Multadd {
+	public static void main(String[] args){
+		multadd(1.0, 2.0, 3.0);
+		multadd(Math.sin(Math.PI / 4), 1, Math.cos(Math.PI / 4) / 2);
+		multadd(Math.log(10), 1, Math.log(20));
+	}
+	public static void multadd(double a, double b, double c){
+		System.out.println(a * b + c);			
+	}
+	public static void expSum(double x){
+		System.out.println(multadd(3, ););
+	}
+
+}
